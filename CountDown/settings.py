e0f0295ebfc39e5d7172c4a85205166dcd92ea5c
Dmytro <foxgunplay@gmail.com>
2021-01-27 18:23:10 +0200

    custom_user configured

--- CountDown/settings.py
+++ CountDown/settings.py
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'custom_user.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
--- custom_user/models.py
+++ custom_user/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-
+from django.contrib.auth.models import PermissionsMixin
 
 class CustomUserManager(BaseUserManager):
 
@@ -19,20 +19,26 @@ class CustomUserManager(BaseUserManager):
 
         user = self.create_user(email, password=password)
         user.is_admin = True
+        user.is_active = True
         user.save(using=self._db)
 
         return user
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True, max_length=256)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
 
-    objects = ''
+    objects = CustomUserManager()
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
     def __str__(self):
         return f'User {self.email}'
