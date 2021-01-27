90754ff4aafff24fe15abfce02e96dbda072f7de
Dmytro <foxgunplay@gmail.com>
2021-01-27 17:45:03 +0200

    app reinitialized

--- CountDown/settings.py
+++ CountDown/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'countdown_core',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
