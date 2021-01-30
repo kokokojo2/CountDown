517cd4bbc3413c76140826408bed7e325400774e
Dmytro <foxgunplay@gmail.com>
2021-01-30 22:43:34 +0200

    custom_user.urls paths included

--- CountDown/urls.py
+++ CountDown/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('countdown/', include('countdown_core.urls', namespace='countdown'))
+    path('countdown/', include('countdown_core.urls', namespace='countdown')),
+    path('', include('custom_user.urls', namespace='users')),
 ]
+
+
+
