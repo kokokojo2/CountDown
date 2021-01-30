17d98392c4b3f4b6c9c8013c538d5c8a981a849d
Dmytro <foxgunplay@gmail.com>
2021-01-30 23:24:15 +0200

    Logout view implemented

--- custom_user/urls.py
+++ custom_user/urls.py
@@ -6,7 +6,10 @@ from .views import SignUpView
 
 app_name = 'custom_user'
 
+# TODO: change a logout template when it`s ready
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='custom_user/login.html'), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='custom_user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='custom_user/logout.html'), name='logout'),
 ]
+
