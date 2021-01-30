3c801a96245ad7984b2873aab7cc69d97ba27e99
Dmytro <foxgunplay@gmail.com>
2021-01-30 23:24:30 +0200

    unused imports deleted

--- custom_user/views.py
+++ custom_user/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, resolve_url
+from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
-from django.contrib.auth import views as auth_views
 
 from .forms import RegistrationForm
 
