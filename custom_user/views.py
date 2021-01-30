0d5be681f0e025babe5c8f20740f2b5b62232ed7
Dmytro <foxgunplay@gmail.com>
2021-01-30 22:41:56 +0200

    SignUpView (handles user registration) created

--- custom_user/views.py
+++ custom_user/views.py
@@ -1,2 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
+from django.views import View
+from django.http import HttpResponse
 from django.contrib.auth import views as auth_views
+
+from .forms import RegistrationForm
+
+
+# TODO: redirect to dashboard if user is already registered
+class SignUpView(View):
+    """
+    Handles an account creation.
+    """
+
+    template_name = 'custom_user/signup.html'
+
+    def post(self, request):
+        registration_form = RegistrationForm(request.POST)
+
+        # TODO: implement redirect to a success url with e-mail confirmation message
+        if registration_form.is_valid():
+            registration_form.save()
+
+            return HttpResponse('The user was successfully created.')
+
+        return render(request, self.template_name, {'form': registration_form})
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': RegistrationForm()})
