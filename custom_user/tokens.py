8a856aa1a13a3903eea293fd9cce5efdee09aa08
Dmytro <foxgunplay@gmail.com>
2021-02-05 19:06:16 +0200

    EmailConfirmationTokenGenerator added

---
+++ custom_user/tokens.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+
+class EmailConfirmationTokenGenerator(PasswordResetTokenGenerator):
+    """
+    Is used to create a token link for email verification.
+    """
+    def _make_hash_value(self, user, timestamp):
+        return str(user.pk) + str(timestamp) + str(user.is_active)
