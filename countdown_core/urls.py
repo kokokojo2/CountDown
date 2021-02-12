fc36b2b09a5ae6b3608a78d32bc1cfcd8b683ed4
Dmytro <foxgunplay@gmail.com>
2021-02-12 20:53:14 +0200

    path to dashboard view added

--- countdown_core/urls.py
+++ countdown_core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CountdownCreateView, CountdownDetailView
+from .views import CountdownCreateView, CountdownDetailView, DashBoardView
 
 app_name = 'countdown_core'
 
 urlpatterns = [
     path('create/', CountdownCreateView.as_view(), name='create'),
-    path('<int:pk>/', CountdownDetailView.as_view(), name='detail')
+    path('<int:pk>/', CountdownDetailView.as_view(), name='detail'),
+    path('dashboard/', DashBoardView.as_view(), name='dashboard'),
 ]
