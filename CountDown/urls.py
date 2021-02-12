c00472c384d2858c2e3bdfcf238232bd3af4d8e7
Dmytro <foxgunplay@gmail.com>
2021-02-12 20:15:58 +0200

    path to HomePageView added

--- CountDown/urls.py
+++ CountDown/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-
+from home.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('countdown/', include('countdown_core.urls', namespace='countdown')),
+    path('', HomePageView.as_view()),
     path('', include('custom_user.urls', namespace='users')),
 ]
 
