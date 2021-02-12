7d6556b0091ab35b30e6422365c12cad703f685f
Dmytro <foxgunplay@gmail.com>
2021-02-12 20:03:08 +0200

    basic homepage view implemented

--- home/views.py
+++ home/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
+
+class HomePageView(View):
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, 'home/homepage_authenticated.html')
+        else:
+            return render(request, 'home/homepage.html')
