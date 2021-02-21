b5ecf0f8e5db601d9cc49a3a073060cb461ba52e
Dmytro <foxgunplay@gmail.com>
2021-02-21 14:16:25 +0200

    HomePageView now returns single page no matter if the user is logged in or not

--- home/views.py
+++ home/views.py
@@ -5,7 +5,4 @@ from django.views import View
 class HomePageView(View):
 
     def get(self, request):
-        if request.user.is_authenticated:
-            return render(request, 'home/homepage_authenticated.html')
-        else:
-            return render(request, 'home/homepage.html')
+        return render(request, 'home/homepage.html')
