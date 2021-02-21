3dc2c3882fc3ab5e370697d7c06567c5d1f83029
Dmytro <foxgunplay@gmail.com>
2021-02-21 14:10:24 +0200

    Forms improved. Now fields has class 'form-control', which makes it visible by bootstrap.

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -1,11 +1,54 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordResetForm, SetPasswordForm
 
 from .models import CustomUser
 
 
 class RegistrationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}),
+    )
+    password2 = forms.CharField(
+        label='Confirm password',
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}),
+        strip=False,
+    )
 
     class Meta:
         model = CustomUser
-        fields = ['email']
+        fields = ['email', 'password1', 'password2']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+
+
+class BootstrapAuthForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control'}))
+    password = forms.CharField(
+        label="Password:",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'form-control'}),
+    )
+
+
+class BootstrapPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(
+        label='Email:',
+        max_length=254,
+        widget=forms.EmailInput(attrs={'autocomplete': 'email', 'class': 'form-control'})
+    )
+
+
+class BootstrapPasswordSetForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label="New password",
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}),
+        strip=False,
+    )
+    new_password2 = forms.CharField(
+        label="New password confirmation",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}),
+    )
