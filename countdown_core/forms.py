8e92664f2b24522ab3bd43117002d0a5734d849f
Dmytro <foxgunplay@gmail.com>
2021-04-06 13:55:45 +0300

    added placeholder to field attributes

--- countdown_core/forms.py
+++ countdown_core/forms.py
@@ -7,8 +7,14 @@ class CountdownForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        for field in self.fields.values():
+        for field_name, field in self.fields.items():
             field.widget.attrs.update({'class': 'form-control'})
+
+        self.fields['description'].widget.attrs.update({'rows': "4",
+                                                        'placeholder': ''}) #'Tell the people what this countdown is made for.'})
+
+        self.fields['finished_text'].widget.attrs.update({'rows': "4",
+                                                          'placeholder': ''}) #'This text will be displayed in the moment, when countdown is finished.'})
 
     class Meta:
         model = Countdown
