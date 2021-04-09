dba001d63ee1967750a946fb7b00d878baa5c24a
Dmytro <foxgunplay@gmail.com>
2021-04-10 01:19:15 +0300

    success login url changed

--- CountDown/settings.py
+++ CountDown/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '../countdown/dashboard/'
 LOGIN_URL = 'login/'
 LOGOUT_REDIRECT_URL = '/'
