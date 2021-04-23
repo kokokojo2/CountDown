6dcfd4c20da97da71a44766e14459247ec6d7175
Dmytro <foxgunplay@gmail.com>
2021-04-23 22:21:53 +0300

    post_reaction reworked due to changes in Response of a service view

--- home/static/reaction.js
+++ home/static/reaction.js
@@ -1,4 +1,4 @@
-function post_reaction(url, trigger_id) {
+function post_reaction(url) {
     const Http = new XMLHttpRequest();
 
     Http.open("GET", url);
@@ -7,21 +7,17 @@ function post_reaction(url, trigger_id) {
     Http.onreadystatechange = (e) => {
         if(Http.readyState === 4 && Http.status === 200)
         {
-            var reactions_number = JSON.parse(Http.responseText);
+            var reaction_data = JSON.parse(Http.responseText);
 
-            var triggered_button = document.getElementById(trigger_id);
-            if(triggered_button.className.includes("blue")) {
-                console.log('like');
-                triggered_button.className = "reaction-btn border round";
-            }
-            else {
-                triggered_button.className += " blue";
-            }
+            document.getElementById("like").className = reaction_data["like_state"] ? "reaction-btn border round blue" : "reaction-btn border round";
+            document.getElementById("dislike").className = reaction_data["dislike_state"] ? "reaction-btn border round blue" : "reaction-btn border round";
+            document.getElementById("laugh").className = reaction_data["laugh_state"] ? "reaction-btn border round blue" : "reaction-btn border round";
+            document.getElementById("cry").className = reaction_data["cry_state"] ? "reaction-btn border round blue" : "reaction-btn border round";
 
-            document.getElementById("laugh-amount").innerText = reactions_number["laugh"];
-            document.getElementById("cry-amount").innerText = reactions_number["cry"];
-            document.getElementById("like-amount").innerText = reactions_number["like"];
-            document.getElementById("dislike-amount").innerText = reactions_number["negative"];
+            document.getElementById("laugh-amount").innerText = reaction_data["laugh"];
+            document.getElementById("cry-amount").innerText = reaction_data["cry"];
+            document.getElementById("like-amount").innerText = reaction_data["like"];
+            document.getElementById("dislike-amount").innerText = reaction_data["negative"];
 
             // probably buttons will have different classes that will help to distinguish current state of reaction
             // if button has class1 (reaction exist) - change it to class2 (reaction doesn't exist)
